f6799b82ece6ca24a4abc45e63a45bdc627ef073
daxtersky <daxtersky@gmail.com>
2022-06-03 11:48:55 +0200

    2 files modified, 1 added, 1 deleted

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,78 @@
-<script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+<script>
+	import Child from './Child.svelte'
+	export let name;
+	let color = 'red'
+	let variable = 'placeholder'
+	let toggleText = true
+	let frameworks = [
+		{id:1, name: 'React'},
+		{id:2, name: 'Angular'},
+		{id:3, name:'Vue'},
+		{id:4, name: 'Svelte'}
+	]
+	const changeColor = () => color = color === 'red' ? 'blue' : 'red'
+	const showText = () => toggleText = !toggleText
+	const addItem = () => {
+		frameworks = [...frameworks, { id: findLastId(frameworks), name: getRandomText() }]
+	}
+	const findLastId = (arr) => arr[arr.length - 1]?.id + 1
+	const getRandomText = () => {
+		let result = ''
+		const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+		for ( var i = 0; i < 7; i++ ) {
+ 		  result += characters.charAt(Math.floor(Math.random() * characters.length));
+ 		}
+ 		return result;
+	}
+	$: watcher = `This is our reactive (watched) variable: ${variable}`
+	$: console.log(`Hello World! 👨‍🌾`);
+	// $: console.log(`The variable is ${variable}`);
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<hr>
+	<Child prop1={variable} {color} />
+	<hr>
+	<input bind:value={variable} />
+	<p>Double binded variable: <strong>{variable}</strong></p>
+	<button on:click={changeColor}>Change color</button>
+	<p style="color: {color}">Watcher / reactive value: {watcher}</p>
+	<button on:click={showText}>Toggle visibility</button>
+	{#if toggleText}
+		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime error impedit qui molestiae enim, illum ut dolor, pariatur est tempore aperiam sunt laudantium illo. Inventore quas quam exercitationem voluptatum aspernatur!
+		Ullam architecto eaque hic natus dolor repellat maiores quam placeat et sequi possimus numquam, culpa aliquam provident atque quisquam excepturi! Labore officia consequatur nihil alias et voluptatum illo delectus non.</p>
+		{:else}
+		<p>Some text hidden</p>
+	{/if}
+	<button on:click={addItem}>Add item</button>
+	{#each frameworks as framework (framework.id)}
+		<ul>
+			<li>{framework.name}</li>
+		</ul>
+	{/each}
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
+	/* :global(h1) { */
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
 
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
 </style>
---
+++ src/Child.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let prop1 = ''
+  export let color = 'black'
+
+  $: console.log('Hello world from child! 👶', );
+</script>
+
+<section>
+  <p style="color: {color}">Hello from child.</p>
+  <p style="color: {color}">Value from parent: <strong>{prop1}</strong></p>
+  
+</section>
+
+<style>
+/* p {
+  color: {color}
+} */
+</style>
