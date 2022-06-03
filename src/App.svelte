88f5c68ac50150b010068d019f3423932ed5d496
daxtersky <daxtersky@gmail.com>
2022-06-03 12:50:08 +0200

    2 files modified

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,68 @@
 <script>
-	import Child from './Child.svelte'
+	import Child from "./Child.svelte";
 	export let name;
-	let color = 'red'
-	let variable = 'placeholder'
-	let toggleText = true
+	let color = "#ff3e00";
+	let variable = "placeholder";
+	let toggleText = true;
 	let frameworks = [
-		{id:1, name: 'React'},
-		{id:2, name: 'Angular'},
-		{id:3, name:'Vue'},
-		{id:4, name: 'Svelte'}
-	]
-	const changeColor = () => color = color === 'red' ? 'blue' : 'red'
-	const showText = () => toggleText = !toggleText
+		{ id: 1, name: "React" },
+		{ id: 2, name: "Angular" },
+		{ id: 3, name: "Vue" },
+		{ id: 4, name: "Svelte" },
+	];
+	let elementsFromChild = []
+	const changeColor = () => color = getRandomColor();
+	const showText = () => (toggleText = !toggleText);
 	const addItem = () => {
-		frameworks = [...frameworks, { id: findLastId(frameworks), name: getRandomText() }]
-	}
-	const findLastId = (arr) => arr[arr.length - 1]?.id + 1
+		frameworks = [
+			...frameworks,
+			{ id: findLastId(frameworks), name: getRandomText() },
+		];
+	};
+	const findLastId = (arr) => arr[arr.length - 1]?.id + 1;
 	const getRandomText = () => {
-		let result = ''
-		const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-		for ( var i = 0; i < 7; i++ ) {
- 		  result += characters.charAt(Math.floor(Math.random() * characters.length));
- 		}
- 		return result;
+		let result = "";
+		const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		for (var i = 0; i < 7; i++) {
+			result += characters.charAt(
+				Math.floor(Math.random() * characters.length)
+			);
+		}
+		return result;
+	};
+	const getRandomColor = () =>  {
+  	let letters = '0123456789ABCDEF';
+  	let color = '#';
+  	for (var i = 0; i < 6; i++) {
+  	  color += letters[Math.floor(Math.random() * 16)];
+  	}
+		console.log('getRandomColor',color );
+  	return color;
 	}
-	$: watcher = `This is our reactive (watched) variable: ${variable}`
-	$: console.log(`Hello World! 👨‍🌾`);
-	// $: console.log(`The variable is ${variable}`);
+	const receiveEventFromChild = (e) => {
+		console.log('event from child received! with param: ', e.detail)
+		elementsFromChild = [...elementsFromChild, +e.detail]
+	};
+
+	$: watcher = `This is our reactive (watched) variable: ${variable}`;
+	$: console.log(`Hello World! 👨‍🌾`); // Called on initialization, creation of the component
+	$: console.log(`Console log shown with each ${variable} change`);
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<hr>
-	<Child prop1={variable} {color} />
-	<hr>
+	<h2 style="color: {color}">Hello {name}! 👨‍🌾</h2>
 	<input bind:value={variable} />
 	<p>Double binded variable: <strong>{variable}</strong></p>
-	<button on:click={changeColor}>Change color</button>
+	<button on:click={changeColor}>Get random color</button>
 	<p style="color: {color}">Watcher / reactive value: {watcher}</p>
 	<button on:click={showText}>Toggle visibility</button>
 	{#if toggleText}
-		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime error impedit qui molestiae enim, illum ut dolor, pariatur est tempore aperiam sunt laudantium illo. Inventore quas quam exercitationem voluptatum aspernatur!
-		Ullam architecto eaque hic natus dolor repellat maiores quam placeat et sequi possimus numquam, culpa aliquam provident atque quisquam excepturi! Labore officia consequatur nihil alias et voluptatum illo delectus non.</p>
-		{:else}
-		<p>Some text hidden</p>
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime error
+			impedit qui molestiae enim, illum ut dolor... 🥸
+		</p>
+	{:else}
+		<p>Here's top secred, well hidden text! 🏴‍☠️</p>
 	{/if}
 	<button on:click={addItem}>Add item</button>
 	{#each frameworks as framework (framework.id)}
@@ -52,22 +70,37 @@
 			<li>{framework.name}</li>
 		</ul>
 	{/each}
+	<p>Elements from child:</p>
+	{#each elementsFromChild as childElement }
+		<span>{childElement}, </span>
+	{/each}
+	<hr />
+	<Child prop1={variable} {color} on:emit-event={receiveEventFromChild} />
 </main>
 
 <style>
+	:global(h2) {
+		font-size: 4em;
+	}
+	:global(h3) {
+		font-size: 2em;
+	}
+	:global(h2),
+	:global(h3) {
+		font-family: Verdana;
+		text-transform: uppercase;
+		font-weight: 100;
+	}
+
+	:global(p) {
+		font-family: Verdana;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	/* :global(h1) { */
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
--- src/Child.svelte
+++ src/Child.svelte
@@ -1,18 +1,36 @@
 <script>
+  import { createEventDispatcher } from 'svelte'
   export let prop1 = ''
   export let color = 'black'
 
+  let childContentVisible = false
+
+  const dispatch = createEventDispatcher()
+
+  const functionWithParam = (val) => randomNumber = getRandomNumber(val)
+  const getRandomNumber = (val = 1000) => {
+    let number = Math.floor(Math.random() * val)
+    return number
+  }
+  const emitEvent = () => dispatch('emit-event', randomNumber)
+  
+  $: randomNumber = getRandomNumber()
   $: console.log('Hello world from child! 👶', );
 </script>
 
 <section>
-  <p style="color: {color}">Hello from child.</p>
+  <h3 style="color: {color}">Hello from child! 👶</h3>
   <p style="color: {color}">Value from parent: <strong>{prop1}</strong></p>
+  <button on:click="{() => childContentVisible = !childContentVisible}">Toogle child content with inline function</button>
+  {#if childContentVisible}
+    <p>Random number: {randomNumber}</p>
+    <small>Each one is smaller than previous one</small>
+    <br>
+    <br>
+    <button on:click={() => functionWithParam(randomNumber) }>Get random number from function with param</button>
+    <!-- <button on:click={() => randomNumber = getRandomNumber() }>Get random number from function with param</button> -->
+    <p>Emit function to parent component</p>
+    <button on:click={emitEvent}>Emit {randomNumber}! 🚀</button>
+  {/if}
   
 </section>
-
-<style>
-/* p {
-  color: {color}
-} */
-</style>
